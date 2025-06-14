b2270ef8d0be55084428e0487d15107f6dd6290f
Mohammad Atif <matif91201@gmail.com>
2025-06-14 17:43:54 +0530

    macos & linux removed

--- pages/extensioninstallation/macos/chrome/_meta.js
+++
@@ -1,6 +0,0 @@
-export default {
-  distributionguide: { title: "Distribution guide" },
-  installmanually: { title: "Install extension manually" },
-  installgoogleadmin: { title: "Install extension via Google Admin" },
-  setuptestdevice: { title: "Setup test device" },
-};
